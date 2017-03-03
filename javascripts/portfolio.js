bdf4b1a8b438e95c5fa1077a9de2a711b556ab60
Marlon Martinez <jmarlonmartinez13@gmail.com>
2017-03-02 23:29:06 -0500

    back-top-top

--- javascripts/portfolio.js
+++ javascripts/portfolio.js
@@ -29,12 +29,24 @@ $(document).ready( () => {
       $('.social').hide(1000);
     }
   });
-      $(window).scroll(function(){
-          if ($(this).scrollTop() > 100) {
-            $('.navbar-inverse').addClass('navbar-fixed-top');
-          } else {
-            $('.navbar-inverse').removeClass('navbar-fixed-top');
-          }
-      });
+  $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+        $('.navbar-inverse').addClass('navbar-fixed-top');
+        $('.back-to-top').show(500);
+      } else {
+        $('.navbar-inverse').removeClass('navbar-fixed-top');
+        $('.back-to-top').hide(500);
+      }
+  });
+
+  $('.back-to-top').on('click', (e) => {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1000);
+    e.preventDefault();
+  });
+
+
+
 
 });
